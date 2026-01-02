d1c6ee6cd0be910fff10107f0129bf28049f25be
Slavetomints <161798204+Slavetomints@users.noreply.github.com>
2026-01-01 23:20:21 -0600

    Initial Commit

---
+++ lib/jekyll/theme/nyx.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "nyx/version"
+
+module Jekyll
+  module Theme
+    module Nyx
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jekyll/theme/nyx/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module Theme
+    module Nyx
+      VERSION = "0.1.0"
+    end
+  end
+end
